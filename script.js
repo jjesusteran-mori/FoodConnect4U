3b80869f773f69fff2d8c967d2fcfa5a8afce128
jesus Teran <iokoumorit@gmai.com>
2020-04-11 13:55:34 -0700

    updated css and html

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // api
+<script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAtqVSPxPNF192v-PmiutR8TLMQhg0C4CU&callback=initMap">
+    </script>
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
