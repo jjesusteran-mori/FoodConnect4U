21867dc766d22b67a91c535caf0f14e3db1e185a
jesus Teran <iokoumorit@gmai.com>
2020-04-11 10:08:45 -0700

    finished address and addressOnly obj/arr

--- address.js
+++ address.js
@@ -147,25 +147,6 @@ var foodLocation = [{
     "website": "https://www.feedoc.org",
     "hours": "N/A"
 },{
-},{
-    "name" : "Stand Up For Kids",
-    "address" : "P.O. Box 14398",
-    "city": "Irvine",
-    "state": "CA",
-    "zip Code": "92623",
-    "phone number": "714-356-5437",
-    "website": "http://www.standupforkids.org",
-    "hours": "Please call first."
-},{
-    "name" : "7 Whatney",
-    "address" : "P.O. Box 14398",
-    "city": "Irvine",
-    "state": "CA",
-    "zip Code": "92618",
-    "phone number": "(949)380-8144",
-    "website": "http://www.sco-oc.org/food-pantry",
-    "hours": "Monday, Wednesday, Thursday, Friday 9:00am - 3:30pm Tuesday 12:30pm - 3:30pm"
-},{
     "name" : "Families Forward",
     "address" : "8 Thomas",
     "city": "Irvine",
@@ -238,7 +219,90 @@ var foodLocation = [{
   "phone number": "714-974-7333",
   "website":"http://www.orangehills.net",
   "hours": "Monday through Thursday 10:00am to 2:00pm"
-}]
+},{
+    "name" : "Marys Kitchen - Orange Cares",
+    "address" : "517 W. Struck Ave",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92867",
+    "phone number": "714-633-0444",
+    "website":"http://www.maryskitchen.org",
+    "hours": " Hot meals Monday - Friday8:00am - 2:00pm Except Wednesday 10:00am - 2:00pm; Clients 9:00 - 3:00pm",
+  },{
+    "name" : "Trinity Guild - La Purisima Catholic Church",
+    "address" : "11712 N. Hewes Street",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92869",
+    "phone number": "(714) 633-5800",
+    "website":"http://www.lapurisima.net/g",
+    "hours": "Pantry Hours: Monday, and Friday 10:00am - 12:00pm 1st, 3rd and 5th Wednesday of the month10:00am - 12:00pm2nd and 4th Wednesday - Mobile Pantry8:30am - 10:30pm(In the parking lot behind the parish office)Thanksgiving Dinners Distribution:Sunday before Thanksgiving",
+  },{
+    "name" : "Friendly Center",
+    "address" : "1820 E Meats Ave.",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92867",
+    "phone number": "(714) 771-5300",
+    "website":"http://www.friendlycenter.org",
+    "hours": "Emergency Food 2nd Friday of the monthMonday and Thursday 10:00am",
+  },{
+    "name" : "Christian Life Fellowship",
+    "address" : "1090 N. Batavia St.",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92867",
+    "phone number": "714-771 9850",
+    "website":"http://www.friendlycenter.org",
+    "hours": "Saturday 9:30am - 11:30am",
+  },
+  {
+    "name" : "Calvary Temple",
+    "address" : "308 S Glassell Street",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92866",
+    "phone number": "714-538-1064",
+    "website":"no website",
+    "hours": " Monday - Friday 9:00am - 5:00pm",
+  },{
+    "name" : "First Presbyterian Church of Orange",
+    "address" : "191 N Orange St",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92866",
+    "phone number": "714-771 9850",
+    "website":"http://www.firstpresorange.org",
+    "hours": "Wednesday10:00am - 12:00pm; 1:00pm - 3:00pmLimit is 2 bags per month per person/household",
+  },
+  {
+    "name" : "You Are Special Food Pantry",
+    "address" : "3512 E. Chapman Ave",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92869",
+    "phone number": "(714) 863-6058",
+    "website":"http://www.youarespecial.com",
+    "hours": "Wednesday 8:15 until 10:00am",
+  },{
+    "name" : "River47 Church",
+    "address" : "250 S. Prospect Street",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92866",
+    "phone number": "(714) 633-6464",
+    "website":"http://www.river47.org",
+    "hours": " It's by appointment only.Hours:Tuesday and Thursday 9:00am - 1:30pm Bring two regular size grocery bags to fill up",
+  },{
+    "name" : "River47 Church",
+    "address" : "250 S. Prospect Street",
+    "city": "Orange",
+    "state": "CA",
+    "zip Code": "92866",
+    "phone number": "(714) 633-6464",
+    "website":"http://www.river47.org",
+    "hours": " It's by appointment only.Hours:Tuesday and Thursday 9:00am - 1:30pm Bring two regular size grocery bags to fill up",
+  }]
 
 console.log(foodLocation.map(function(a){
     return a['address']
