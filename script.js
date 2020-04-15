a63a8d5adf13de07969daff412c1918837c73e8c
jesus Teran <iokoumorit@gmai.com>
2020-04-14 19:39:29 -0700

    map section now contains orange county map section

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 
 // api
 
+function initMap() {
+    // The location of Uluru 33.6394877,-118.3298942
+    var uluru = {lat: 33.7175, lng: -117.8311};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 11, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
+  }
+
