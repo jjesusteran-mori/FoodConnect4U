1601c2d019a874c771e65ed77014eea5b296ae6d
unknown <lfreeman0820@gmail.com>
2020-04-16 20:14:07 -0700

    ok

--- address.js
+++ address.js
@@ -7,47 +7,29 @@ var foodLocation = [{
     "address" : "1611 N. Broadway",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92706",
-    "lat & lon": "33.7592574,-117.8714787",    
-    "phone number": "(714) 547-0615",
-=======
     "zipCode": "92706",
     "latLong": "33.7592574,-117.8714787",    //latLong.split('') => ["33.7592574","-117.8714787"]
     "phoneNumber": "(714) 547-0615",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.veteransfirstoc.org",
-    "hours": "Monday and Friday7:00 am - 4:00 pm"
+    "hours": "Monday and Friday 7:00 am - 4:00 pm"
 },{
     "name" : "St Vincent de Paul St Joseph",
     "address" : "608 Civic Center East",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92701",
-    "lat & lon": "33.7518928,-117.8639407", 
-    "phone number": "(714)-550-7101",
-=======
     "zipCode": "92701",
     "latLong": "33.7518928,-117.8639407", 
-    "phoneNumber": "(714)-550-7101",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
+    "phoneNumber": "(714) 550-7101",
     "website":"http://www.stjosephsa.org",
-    "hours": "Hot meals Saturday9:00am - 1:00pm"
+    "hours": "Hot meals Saturday 9:00am - 1:00pm"
 },{
     "name" : "Serve The People",
     "address" : "1206 E 17th St, Suite 204",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92701",
-    "lat & lon": "33.7595,-117.8562717",
-    "phone number": "(714)-352-2911",
-=======
     "zipCode": "92701",
     "latLong": "33.7595,-117.8562717",
-    "phoneNumber": "(714)-352-2911",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
+    "phoneNumber": "(714) 352-2911",
     "website": "http://www.serve-the-people.com",
     "hours": "1st and 3rd Saturday of the month 6:00am - 10am Early registration at 4:30am"
 },{
@@ -55,15 +37,9 @@ var foodLocation = [{
     "address" : "1710 W Edinger Ave",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92704",
-    "lat & lon": "33.7269158,-117.8934462",
-    "phone number": "(714) 545-2937",
-=======
     "zipCode": "92704",
     "latLong": "33.7269158,-117.8934462",
     "phoneNumber": "(714) 545-2937",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://santaana.salvationarmy.org",
     "hours": "Everyday 7:00am - 7:00pm"
 },{
@@ -71,15 +47,9 @@ var foodLocation = [{
     "address" : "1622 West 6th Street",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92703",
-    "lat & lon": "33.7494028,-117.8929884",
-    "phone number": "714-543-1700",
-=======
     "zipCode": "92703",
     "latLong": "33.7494028,-117.8929884",
-    "phoneNumber": "714-543-1700",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
+    "phoneNumber": "(714) 543-1700",
     "website": "N/A",
     "hours": "Friday 2:30pm - 3:30pm"
 },{
@@ -87,15 +57,9 @@ var foodLocation = [{
     "address" : "3135 Warner Ave",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92704",
-    "lat & lon": "33.7156565,-117.913488",
-    "phone number": "(714)-979-5483",
-=======
     "zipCode": "92704",
     "latLong": "33.7156565,-117.913488",
-    "phoneNumber": "(714)-979-5483",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
+    "phoneNumber": "(714) 979-5483",
     "website": "N/A",
     "hours": "3rd Saturday of the month 1:00pm - 3:00pm"
 },{
@@ -103,15 +67,9 @@ var foodLocation = [{
     "address" : "3117 West McFadden Ave",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92707",
-    "lat & lon": "33.7353007,-117.9125629",
-    "phone number": "(714)-864-6410",
-=======
     "zipCode": "92707",
     "latLong": "33.7353007,-117.9125629",
     "phoneNumber": "(714)-864-6410",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "www.santaanamcfadden.org",
     "hours": "2nd and 4th Wednesday of every month 6:30pm -7:00pm"
 },{
@@ -119,15 +77,9 @@ var foodLocation = [{
     "address" : "940 West Wilshire Avenue",
     "city": "Santa Ana",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92707",
-    "lat & lon": "33.730428,-117.8807198",
-    "phone number": "(714) 850-1952",
-=======
     "zipCode": "92707",
     "latLong": "33.730428,-117.8807198",
     "phoneNumber": "(714) 850-1952",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "N/A",
     "hours": "Tuesday 6-8pm"
 },{
@@ -135,15 +87,9 @@ var foodLocation = [{
     "address" : "1531 S. Sinclair St.",
     "city": "Anaheim",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92806",
-    "lat & lon": "33.8101369,-117.8828257",
-    "phone number": "(714)-937-5111",
-=======
     "zipCode": "92806",
     "latLong": "33.8101369,-117.8828257",
     "phoneNumber": "(714)-937-5111",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.stadiumvineyard.org",
     "hours": "Sunday 12:15pm to 1:00pm"
 },{
@@ -151,15 +97,9 @@ var foodLocation = [{
     "address" : "295 E Orangethorpe Ave",
     "city": "Anaheim",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92801",
-    "lat & lon": "33.861302,-117.9170196",
-    "phone number": "(714) 526-8233",
-=======
     "zipCode": "92801",
     "latLong": "33.861302,-117.9170196",
     "phoneNumber": "(714) 526-8233",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.gototherock.com",
     "hours": "Saturday7:00am - 8:45am"
 },{
@@ -167,15 +107,9 @@ var foodLocation = [{
     "address" : "2050 W. Ball Road",
     "city": "Anaheim",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92804",
-    "lat & lon": "33.8173226,-117.9557374",
-    "phone number": "(714) 774-2595",
-=======
     "zipCode": "92804",
     "latLong": "33.8173226,-117.9557374",
     "phoneNumber": "(714) 774-2595",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.saintjustin.org",
     "hours": "Saturday7:00am - 8:45am"
 },{
@@ -183,15 +117,9 @@ var foodLocation = [{
     "address" : "2780 E Wagner Ave",
     "city": "Anaheim",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92806",
-    "lat & lon": "33.8248559,-117.8739443",
-    "phone number": "(714)-632-6838",
-=======
     "zipCode": "92806",
     "latLong": "33.8254395,-117.8719061",
     "phoneNumber": "(714)-632-6838",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "N/A",
     "hours": "Call for times"
 },{
@@ -199,15 +127,9 @@ var foodLocation = [{
     "address" : "2180 West Crescent Ave",
     "city": "Anaheim",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92801",
-    "lat & lon": "33.8248559,-117.8739443",
-    "phone number": "(714) 399-4571",
-=======
     "zipCode": "92801",
     "latLong": "33.8248559,-117.8739443",
     "phoneNumber": "(714) 399-4571",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": " http://www.crescentclinicoc.org",
     "hours": "Tuesday and Wednesday 10:00am - 2:00pm"
 },{
@@ -215,15 +137,9 @@ var foodLocation = [{
     "address" : "2950 E Imperial Hwy",
     "city": "Brea",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92801",
-    "lat & lon": "33.9103921,-117.8599804",
-    "phone number": "714-982-5055",
-=======
     "zipCode": "92801",
     "latLong": "33.9103921,-117.8599804",
     "phoneNumber": "714-982-5055",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.southlands.net",
     "hours": "Last Saturday of every other month - April, June, August, October, December, February"
 },{
@@ -231,15 +147,9 @@ var foodLocation = [{
     "address" : "585 S. Walnut Ave",
     "city": "Brea",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92821",
-    "lat & lon": "33.910546,-117.907362",
-    "phone number": "(714) 529-1821",
-=======
     "zipCode": "92821",
     "latLong": "33.910546,-117.907362",
     "phoneNumber": "(714) 529-1821",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://stangelabreachurch.org",
     "hours": "Saturday8:30am - Breakfast 10:30am - Hot Meal"
 },{
@@ -247,15 +157,9 @@ var foodLocation = [{
     "address" : "8014 Marine Way",
     "city": "Irvine",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92618",
-    "lat & lon": "33.6619043,-117.7404423",
-    "phone number": "(949)653-2900",
-=======
     "zipCode": "92618",
     "latLong": "33.6619043,-117.7404423",
     "phoneNumber": "(949)653-2900",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "https://www.feedoc.org",
     "hours": "N/A"
 },{
@@ -263,15 +167,9 @@ var foodLocation = [{
     "address" : "8 Thomas",
     "city": "Irvine",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92618",
-    "lat & lon": "33.6437725,-117.7236372",
-    "phone number": "(949) 552-2727",
-=======
     "zipCode": "92618",
     "latLong": "33.6437725,-117.7236372",
     "phoneNumber": "(949) 552-2727",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website": "http://www.families-forward.org",
     "hours": "For more information, please call."
 },
@@ -280,15 +178,9 @@ var foodLocation = [{
     "address" : "710 S Cambridge Street",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92866",
-    "lat & lon": "33.7766806,-117.8477096",
-    "phone number": "(714) 997-8881",
-=======
     "zipCode": "92866",
     "latLong": "33.7766806,-117.8477096",
     "phoneNumber": "(714) 997-8881",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.zcc.org",
     "hours": "1st and 3rd Wednesday of each month 10:00am - 1:00pm"
 },{
@@ -296,15 +188,9 @@ var foodLocation = [{
   "address" : "300 E. Taft Ave",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92865",
-  "lat & lon": "33.8154168,-117.8521389",
-  "phone number": "(714) 637-4360",
-=======
   "zipCode": "92865",
   "latLong": "33.8154168,-117.8521389",
   "phoneNumber": "(714) 637-4360",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"http://www.stnorbertchurch.org/index.html",
   "hours": " Saturday Hot Meal 12:30pm - 2:00pm On call 7 days"
 },{
@@ -312,15 +198,9 @@ var foodLocation = [{
   "address" : "154 S. Shaffer St.",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92866",
-  "lat & lon": "33.7868265,-117.8511436",
-  "phone number": "714-228-8440",
-=======
   "zipCode": "92866",
   "latLong": "33.7868265,-117.8511436",
   "phoneNumber": "714-228-8440",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"http://www.stjohnsorange.org",
   "hours": "Saturday 8:00am - 10:00am (closed last Saturday of the month)"
 },{
@@ -328,15 +208,9 @@ var foodLocation = [{
   "address" : "1125 W Alvarez Ave.",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92868",
-  "lat & lon": "33.7982249,-117.8670397",
-  "phone number": "714-588-1577",
-=======
   "zipCode": "92868",
   "latLong": "33.7982249,-117.8670397",
   "phoneNumber": "714-588-1577",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"http://www.praisechapel.com",
   "hours": "Thursday 5:00pm"
 },{
@@ -344,15 +218,9 @@ var foodLocation = [{
   "address" : "250 S. Prospect Ave.",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92869",
-  "lat & lon": "33.7645702,-117.8383178",
-  "phone number": "714-633-6464x10",
-=======
   "zipCode": "92869",
   "latLong": "33.7645702,-117.8383178",
   "phoneNumber": "714-633-6464x10",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"hhttp://www.river47.org",
   "hours": "Call for appointment once a month.Hours:Tuesday and Thursday9:00am - 2:00pm",
 },{
@@ -360,15 +228,9 @@ var foodLocation = [{
   "address" : "170 S Olive St",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92866",
-  "lat & lon": "33.8352629,-117.9117485",
-  "phone number": "(714) 538-9633",
-=======
   "zipCode": "92866",
   "latLong": "33.8352629,-117.9117485",
   "phoneNumber": "(714) 538-9633",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"http://www.orangeseniorcenter.org",
   "hours": " Seniors only. Pantry Hours: Emergency Food 3rd Tuesday of the month 9:00amFor more information, please call."
 },{
@@ -376,15 +238,9 @@ var foodLocation = [{
   "address" : "2910 N Santiago Blvd.",
   "city": "Orange",
   "state": "CA",
-<<<<<<< HEAD
-  "zip Code": "92867",
-  "lat & lon": "33.8379946,-117.8363079",
-  "phone number": "714-974-7333",
-=======
   "zipCode": "92867",
   "latLong": "33.8379946,-117.8363079",
   "phoneNumber": "714-974-7333",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
   "website":"http://www.orangehills.net",
   "hours": "Monday through Thursday 10:00am to 2:00pm"
 },{
@@ -392,15 +248,9 @@ var foodLocation = [{
     "address" : "517 W. Struck Ave",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92867",
-    "lat & lon": "33.8061452,-117.8597193",
-    "phone number": "714-633-0444",
-=======
     "zipCode": "92867",
     "latLong": "33.8061452,-117.8597193",
     "phoneNumber": "714-633-0444",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.maryskitchen.org",
     "hours": " Hot meals Monday - Friday8:00am - 2:00pm Except Wednesday 10:00am - 2:00pm; Clients 9:00 - 3:00pm",
   },{
@@ -408,15 +258,9 @@ var foodLocation = [{
     "address" : "11712 N. Hewes Street",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92869",
-    "lat & lon": "33.7919979,-117.8074344",
-    "phone number": "(714) 633-5800",
-=======
     "zipCode": "92869",
     "latLong": "33.7919979,-117.8074344",
     "phoneNumber": "(714) 633-5800",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.lapurisima.net/g",
     "hours": "Pantry Hours: Monday, and Friday 10:00am - 12:00pm 1st, 3rd and 5th Wednesday of the month10:00am - 12:00pm2nd and 4th Wednesday - Mobile Pantry8:30am - 10:30pm(In the parking lot behind the parish office)Thanksgiving Dinners Distribution:Sunday before Thanksgiving",
   },{
@@ -424,15 +268,9 @@ var foodLocation = [{
     "address" : "1820 E Meats Ave.",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92867",
-    "phone number": "(714) 771-5300",
-    "lat & lon": "33.8221751,-117.8368574",
-=======
     "zipCode": "92867",
     "phoneNumber": "(714) 771-5300",
     "latLong": "33.8221751,-117.8368574",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.friendlycenter.org",
     "hours": "Emergency Food 2nd Friday of the monthMonday and Thursday 10:00am",
   },{
@@ -440,15 +278,9 @@ var foodLocation = [{
     "address" : "1090 N. Batavia St.",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92867",
-    "phone number": "714-771 9850",
-    "lat & lon": "33.8050983,-117.8663647",
-=======
     "zipCode": "92867",
     "phoneNumber": "714-771 9850",
     "latLong": "33.8050983,-117.8663647",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.friendlycenter.org",
     "hours": "Saturday 9:30am - 11:30am",
   },
@@ -457,15 +289,9 @@ var foodLocation = [{
     "address" : "308 S Glassell Street",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92866",
-    "lat & lon": "33.7840586,-117.855607",
-    "phone number": "714-538-1064",
-=======
     "zipCode": "92866",
     "latLong": "33.7840586,-117.855607",
     "phoneNumber": "714-538-1064",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"no website",
     "hours": " Monday - Friday 9:00am - 5:00pm",
   },{
@@ -473,15 +299,9 @@ var foodLocation = [{
     "address" : "191 N Orange St",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92866",
-    "lat & lon": "33.7893094,-117.8536619",
-    "phone number": "714-771 9850",
-=======
     "zipCode": "92866",
     "latLong": "33.7893094,-117.8536619",
     "phoneNumber": "714-771 9850",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.firstpresorange.org",
     "hours": "Wednesday10:00am - 12:00pm; 1:00pm - 3:00pmLimit is 2 bags per month per person/household",
   },
@@ -490,15 +310,9 @@ var foodLocation = [{
     "address" : "3512 E. Chapman Ave",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92869",
-    "lat & lon": "33.7875541,-117.8179314",
-    "phone number": "(714) 863-6058",
-=======
     "zipCode": "92869",
     "latLong": "33.7875541,-117.8179314",
     "phoneNumber": "(714) 863-6058",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.youarespecial.com",
     "hours": "Wednesday 8:15 until 10:00am",
   },{
@@ -506,15 +320,9 @@ var foodLocation = [{
     "address" : "250 S. Prospect Street",
     "city": "Orange",
     "state": "CA",
-<<<<<<< HEAD
-    "zip Code": "92866",
-    "lat & lon": "33.785649,-117.8210131",
-    "phone number": "(714) 633-6464",
-=======
     "zipCode": "92866",
     "latLong": "33.785649,-117.8210131",
     "phoneNumber": "(714) 633-6464",
->>>>>>> fe5b19cdee38c159ee14876c96b97f49684cfc11
     "website":"http://www.river47.org",
     "hours": " It's by appointment only.Hours:Tuesday and Thursday 9:00am - 1:30pm Bring two regular size grocery bags to fill up",
   },]
